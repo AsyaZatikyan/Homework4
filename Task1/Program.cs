bff296790a3035760de59e1ed1f0aaeba4926997
Aghavni Zatikyan <asya.zatikyan.24@gmail.com>
2023-02-10 12:21:33 +0300

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ Напишите цикл, который принимает на вход два числа (A и B) 
+ и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+int Getnumber(string massege)
+{
+  int result = 0;
+  while (true)
+  {
+    Console.WriteLine(massege);
+    if(int.TryParse(Console.ReadLine(), out result))
+    {
+      break;
+    }
+    else
+    {
+      Console.WriteLine("Ввели не число, попробуйте еще раз: ");
+    }
+  }
+  return result;
+}
+
+int Exponentiation(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+
+    return result;
+}
+
+  int numberA =(Getnumber("Введите число A: "));
+  int numberB =(Getnumber("Введите число B: "));
+
+
+  int exponentiation = Exponentiation(numberA, numberB);
+  Console.WriteLine("Ответ: " + exponentiation);
+
